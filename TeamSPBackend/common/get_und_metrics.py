d05f631274c977218efec06ab274aa06a1365937
Bingrui <bingruih@student.unimelb.edu.au>
2021-09-24 20:17:28 +1000

    Modified command line path of SciTools for Windows machine

--- TeamSPBackend/common/get_und_metrics.py
+++ TeamSPBackend/common/get_und_metrics.py
@@ -1,3 +1,4 @@
+import understand
 import json
 import sys
 import os
@@ -8,20 +9,28 @@ import os
 # UND_PATH = '/Applications/Understand.app/Contents/MacOS/'
 
 # For Linux Server
-UND_PATH = '~/comp90082sp/understand/scitools/bin/linux64/'
+#UND_PATH = '~/comp90082sp/understand/scitools/bin/linux64/'
+
+# For Windows
+UND_PATH = 'D:\\SciTools\\bin\\pc-win64'
+
 sys.path.append(UND_PATH)
 sys.path.append(UND_PATH+'Python')
-import understand
 
+# '/home/ec2-user/comp90082sp/COMP90082_Software_Project_Database_Backend/'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-UND_FILE_PATH = os.path.dirname(BASE_DIR)  # '/home/ec2-user/comp90082sp/COMP90082_Software_Project_Database_Backend/'
-METRICS_FILE_PATH = BASE_DIR + '/resource/understand/'
+UND_FILE_PATH = os.path.dirname(BASE_DIR)
+# METRICS_FILE_PATH = BASE_DIR + '/resource/understand/'
+METRICS_FILE_PATH = BASE_DIR + "\\resource\\understand\\"
+
 
 if __name__ == '__main__':
     und_file_name = sys.argv[1]
     metrics_file_name = sys.argv[2]
-    und_file = UND_FILE_PATH +"/"+ und_file_name
-    metrics_file = METRICS_FILE_PATH + metrics_file_name
+    und_file = UND_FILE_PATH + "/" + und_file_name
+    # metrics_file = METRICS_FILE_PATH + metrics_file_name
+    metrics_file = metrics_file_name
+
     # print('BASE_DIR : ', BASE_DIR)
     # print('und_file : ', und_file)
     # print('metrics_file : ', metrics_file)
