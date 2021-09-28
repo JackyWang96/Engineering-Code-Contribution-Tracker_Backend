491733c6b228e4aa71ff62d85367d550c1c0c5a5
Wxxu1 <kimchybob@gmail.com>
2021-09-28 15:48:25 +1000

    add one more git url

--- TeamSPBackend/project/models.py
+++ TeamSPBackend/project/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class ProjectCoordinatorRelation(models.Model):
     coordinator_id = models.IntegerField(null=False)
     space_key = models.CharField(max_length=256, null=False)
-    git_url = models.CharField(max_length=256, null=False)
+    git_url = models.CharField(max_length=256, null=False, default="")
+    git_backend_url = models.CharField(max_length=256, null=False, default="")
     jira_project = models.CharField(max_length=256, null=False)
     git_username = models.CharField(max_length=256, null=False, default="")
     git_password = models.CharField(max_length=256, null=False, default="")
